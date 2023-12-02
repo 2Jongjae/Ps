1f7b8bfc9c476e01e4533205ffc19b325d8ab199
JongJae2 <149658209+JongJae2@users.noreply.github.com>
2023-12-02 21:25:32 +0900

    [Silver III] Title: 등수 매기기, Time: 684 ms, Memory: 54244 KB -BaekjoonHub

--- 백준/Silver/2012. 등수 매기기/등수 매기기.java
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -2,22 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws NumberFormatException, IOException {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	int n = Integer.parseInt(br.readLine());
+	
+	PriorityQueue<Integer> li = new PriorityQueue<>();
+	for (int i = 0 ; i< n ; i ++) {
+		li.add(Integer.parseInt(br.readLine()));
+	}
+	long sum = 0;
 
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-        
-        Arrays.sort(arr); 
-
-        long sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += Math.abs(arr[i] - (i + 1));
-        }
-
-        System.out.println(sum);
-    }
+	
+	for ( int i = 0 ; i< n ; i++) {
+		int temp = li.poll();
+		sum += Math.abs( temp - (i+1));
+	}
+	
+	System.out.println(sum);
+	}
 }
