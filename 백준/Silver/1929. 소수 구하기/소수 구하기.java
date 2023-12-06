14efd10e211343ef2e16f2113d0dcf5af795f9c9
JongJae2 <149658209+JongJae2@users.noreply.github.com>
2023-12-06 20:32:28 +0900

    [Silver III] Title: 소수 구하기, Time: 320 ms, Memory: 23208 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
+
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        for (int i = m; i <= n; i++) {
+            if (isPrime[i]) {
+                sb.append(i).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
