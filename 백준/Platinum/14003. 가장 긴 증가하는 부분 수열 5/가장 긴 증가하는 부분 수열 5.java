ef5d1c6a661fff15da507b8b25d5c9a63b47fafb
JongJae2 <149658209+JongJae2@users.noreply.github.com>
2023-12-13 13:05:30 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 1048 ms, Memory: 225644 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
@@ -1,70 +1,38 @@
 import java.io.*;
 import java.util.*;
-class position{
-	int idx, val;
-	
-	public position(int idx, int val) {
-		super();
-		this.idx = idx;
-		this.val = val;
-	}
-	
-}
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		
 		int n = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
 
 		List<Integer> li =  new ArrayList<Integer>();
-		int[] tt = new int[n];
+		int[] index = new int[n];
 		int[] value = new int[n];
-		
-		int start = -1 ;
-		int last = -1 ;
 		
 		for ( int i = 0 ; i < n ; i ++) {
 			int temp = Integer.parseInt(st.nextToken());
 			value[i] = temp;
 			if( li.size() == 0 || li.get(li.size()-1) < temp ) {
 				li.add(temp);
-				last = i ; 
-				
-				if( li.size() ==  1 ) {
-					tt[i] = 0;
-					start = i;
-				}
-				else tt[i] = li.size()-1;
+				index[i] = li.size()-1;
 				continue;
 			}
-			
 			int idx = Collections.binarySearch(li, temp);
 			if( idx < 0 ) idx = -idx -1;
-			tt[i] = idx;
-			
+			index[i] = idx;
 			if ( idx == 0 && temp == li.get(0) ) continue;
-			if  (idx == 0 ) start  = i ;
 			li.set(idx, temp);
-			
-			
-		} // for 문 
+		}
 		
 		sb.append(li.size() + "\n");
-
-//		for ( int a : li) {
-//			sb.append(a + " ");
-//		}
-		
-		
 		List<Integer> result = new LinkedList<Integer>();
 		
 		int size = li.size()-1;
 		for ( int i = n-1;  i>=0 ; i--) {
-			if( tt[i] == size) {
+			if( index[i] == size) {
 				result.add(value[i]);
 				size--;
 			}
@@ -72,13 +40,6 @@ public class Main {
 		Collections.reverse(result);
 		
 		for ( int a : result) sb.append(a + " ");
-//		System.out.println(result);
-		
-		
-		
-//		System.out.println( start);
-//		System.out.println( last);
-//		System.out.println(Arrays.toString(tt));
 		System.out.println(sb.toString().trim());
 		
 	}
