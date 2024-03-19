ea81bc22516c616d67232d8c2a4b05fbd9a65554
이종재 <149658209+JongJae2@users.noreply.github.com>
2024-03-19 20:31:31 +0900

    [Gold I] Title: 벽 부수고 이동하기 3, Time: 4432 ms, Memory: 672616 KB -BaekjoonHub

---
+++ 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
@@ -0,0 +1,100 @@
+import java.util.*;
+import java.io.*;
+
+class Position{
+	int x,y,cnt;
+	Position ( int x, int y,  int cnt){
+		this.x = x;
+		this.y = y;
+		this.cnt = cnt;
+	}
+	@Override
+	public String toString() {
+		return "Position [x=" + x + ", y=" + y + ", cnt=" + cnt + "]";
+	}
+	
+}
+
+
+class Main {
+	static int[][] arr;
+	static int n,m,k;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		arr = new int[n][m];
+		for(int i = 0 ; i < n ; i ++) {
+			String input = br.readLine();
+			for (int j = 0 ; j < m ; j++) {
+				arr[i][j] = input.charAt(j) - '0';
+			}
+		}
+		
+		Queue<Position> qq = new ArrayDeque<>();
+		qq.add( new Position(0,0,0));
+		
+		boolean[][][][] visit = new boolean[n][m][k+1][2];
+		int[] deltax = {-1,1,0,0};
+		int[] deltay = {0,0,-1,1};
+		visit[0][0][0][0] = true;
+		char day = 'n';
+		int ans = 0;
+		while(!qq.isEmpty()) {
+			ans++;
+			int size = qq.size();
+//			System.out.println(size);
+//			System.out.println(ans);
+//			System.out.println("아침은 = n 밤은 = m " + day + " 걸린 시간 " + ans);
+			for(int i = 0 ; i< size ; i++) {
+				Position temp = qq.poll();
+				int x = temp.x;
+				int y = temp.y;
+				int cnt = temp.cnt;
+//				System.out.println(temp);
+				if( x == n-1 && y == m-1) {
+					System.out.println(ans);
+					return;
+				}
+				for( int j = 0 ; j < 4 ; j++) {
+					int xx = x + deltax[j];
+					int yy = y + deltay[j];
+					
+					if(xx>=0 && xx< n && yy>=0 && yy< m ) {
+						if(arr[xx][yy] == 0  && !visit[xx][yy][cnt][0]) {
+							qq.add(new Position(xx,yy,cnt));
+							visit[xx][yy][cnt][0]= true;
+							continue;
+						}
+						if(arr[xx][yy] == 1 && cnt+1 <=k  ) {
+							if( day == 'm' && !visit[xx][yy][cnt+1][1]) {
+								qq.add(temp);
+								visit[xx][yy][cnt+1][1] = true;
+								continue;
+							}
+							if( day == 'n' && !visit[xx][yy][cnt+1][0]) {
+								qq.add(new Position(xx,yy,cnt+1));
+								visit[xx][yy][cnt+1][0] = true; 
+								continue;
+							}
+							
+						}
+						
+					}
+				}
+		
+				
+			} // size
+			if(day == 'm' ) day = 'n';
+			else day ='m';
+			
+		} //while
+		System.out.println(-1);
+		
+	}
+	
+}
