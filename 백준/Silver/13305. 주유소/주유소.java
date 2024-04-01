98b978d597c65b57cd5a62a109c1ffa278736bc1
이종재 <149658209+JongJae2@users.noreply.github.com>
2024-04-01 10:11:26 +0900

    [Silver III] Title: 주유소, Time: 376 ms, Memory: 35468 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -11,23 +11,16 @@ public class Main {
 		int[] cost=  new int[n-1];
 		int[] arr = new int[n-1];
 		
-		int idx = -1;
-		int min = Integer.MAX_VALUE;
+		
 		st = new StringTokenizer(br.readLine());
 		for( int i = 0 ; i< n-1 ; i++)  cost[i] = Integer.parseInt(st.nextToken());
 		
 		st = new StringTokenizer(br.readLine());
-		for( int i = 0 ; i< n-1 ; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-			if( min > arr[i]) {
-				idx = i;
-				min =arr[i];
-			}
-		}
+		for( int i = 0 ; i< n-1 ; i++)  arr[i] = Integer.parseInt(st.nextToken());
 		
 		long ans =0;
 
-		min = Integer.MAX_VALUE;
+		long min = Long.MAX_VALUE;
 		for (int i =  0 ; i < n-1 ; i++) {
 			if(arr[i] <min) {
 				min =  arr[i];
