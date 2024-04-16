3416c64eef89fc6ffcf6e993a59efac4159f77df
이종재 <149658209+JongJae2@users.noreply.github.com>
2024-04-16 09:22:13 +0900

    [Silver IV] Title: 국영수, Time: 1648 ms, Memory: 66156 KB -BaekjoonHub

---
+++ 백준/Silver/10825. 국영수/국영수.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+
+class Position implements Comparable<Position>{
+	String name;
+	int ko,en,ma;
+	Position(String name, int ko, int en, int ma){
+		this.name = name;
+		this.ko = ko;
+		this.en = en;
+		this.ma = ma;
+	}
+	public int compareTo(Position o) {
+		if( this.ko == o.ko) {
+			if( this.en == o.en) {
+				if(this.ma == o.ma) return this.name.compareTo(o.name);
+				return -(this.ma-o.ma);
+			}
+			return -(o.en -this.en);
+		}
+		return -(this.ko - o.ko);
+	}
+}
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		TreeSet<Position> ts = new TreeSet<>();
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i = 0 ; i< n ; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String name = st.nextToken();
+			int ko = Integer.parseInt(st.nextToken());
+			int en = Integer.parseInt(st.nextToken());
+			int ma = Integer.parseInt(st.nextToken());
+			ts.add(new Position(name,ko,en,ma));
+		}
+
+		while(!ts.isEmpty()) System.out.println(ts.pollFirst().name);
+	}
+
+}
