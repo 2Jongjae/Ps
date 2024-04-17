72e73fda7fbc4a88d73dd7e868464bc3eefdc2b2
이종재 <149658209+JongJae2@users.noreply.github.com>
2024-04-17 20:59:57 +0900

    [Silver I] Title: 회의실 배정, Time: 556 ms, Memory: 43996 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,43 +1,44 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Position implements Comparable<Position>{
-	int x, y;
-	Position ( int x,  int y ){
-		this.x = x;
-		this.y = y;
+	int start, end;
+	Position(int start, int end){
+		this.start = start;
+		this.end = end;
 	}
+	
 	public int compareTo(Position o) {
-		if(this.y == o.y ) return this.x -o.x;
-		return this.y - o.y;
+		if( this.end== o.end) return this.start-o.start; 
+		return this.end-o.end;
 	}
-	
 }
-
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		
-		List<Position> li = new ArrayList<>();
-		for( int i = 0 ; i < n ; i ++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			li.add(new Position(a,b));
-		}
-		
-		Collections.sort(li);
-		
-		List<Position> ans = new ArrayList<>();
-		ans.add(li.get(0));
-		
-		for( int i = 1 ; i < n ; i ++) {
-			if(li.get(i).x >= ans.get(ans.size()-1).y) ans.add(li.get(i));
-		}
-		System.out.println(ans.size());
-		
-	}
-	
+    public static void main(String[] args) throws Exception{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	int n = Integer.parseInt(br.readLine());
+    	
+    	Position[] arr = new Position[n];
+    	
+    	for( int i = 0 ; i < n ; i++) {
+    		StringTokenizer st = new StringTokenizer(br.readLine());
+    		int start = Integer.parseInt(st.nextToken());
+    		int end = Integer.parseInt(st.nextToken());
+    		arr[i] = new Position(start,end);
+    	}
+    	
+    	Arrays.sort(arr);
+    	
+    	List<Position> li = new ArrayList<>();
+    	li.add(arr[0]);
+    	
+    	for(int i = 1 ;  i < n ; i++) {
+    		if(arr[i].start >= li.get(li.size()-1).end) {
+    			li.add(arr[i]);
+    		}
+    	}
+    	System.out.println(li.size());
+    	
+    }
 }
