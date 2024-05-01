69a4ca925aaf3059cf46195de6a4576acc9e8adb
이종재 <149658209+JongJae2@users.noreply.github.com>
2024-05-01 21:19:16 +0900

    [Gold IV] Title: 트리, Time: 596 ms, Memory: 71572 KB -BaekjoonHub

---
+++ 백준/Gold/4803. 트리/트리.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int Tc = 1;
+		while(true) {
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			if( n== 0 && m ==0 ) break;
+			ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+			
+			for( int i = 0 ; i<= n ; i++) graph.add(new ArrayList<>());
+			
+			for (int i = 0 ; i < m ; i ++) {
+				st = new StringTokenizer(br.readLine());
+				int start = Integer.parseInt(st.nextToken());
+				int end = Integer.parseInt(st.nextToken());
+				
+				graph.get(start).add(end);
+				graph.get(end).add(start);
+			}
+			boolean[] visit = new boolean[n+1];
+			int[] parent = new int[n+1];
+			Queue<Integer> qq = new ArrayDeque<>();
+			
+			int ans = 0 ;
+			
+			for( int i = 1 ; i <= n ; i++) {
+				if(!visit[i]) {
+					boolean flag = false;
+					ans++;
+					qq.add(i);
+					visit[i] = true;
+					while(!qq.isEmpty()) {
+						int temp =qq.poll();
+						for ( int adj : graph.get(temp)) {
+							if( !visit[adj]) {
+								parent[adj] = temp;
+								qq.add(adj);
+								visit[adj] = true;
+							} else if(parent[temp] != adj) {
+//								System.out.println("==================");
+//								System.out.println(adj);
+//								System.out.println("==================");
+								flag =true;
+							}
+						}
+					}
+					if(flag) ans--;
+				}
+			}
+			
+			if( ans == 1 ) sb.append("Case " + Tc + ": There is one tree."+"\n");
+			if( ans > 1 ) sb.append("Case " + Tc + ": A forest of "+ans+" trees."+"\n");
+			if( ans == 0 ) sb.append("Case " + Tc + ": No trees."+"\n");
+			Tc++;
+//			for(int i = 1 ; i<=n ; i ++) {
+//				System.out.println(graph.get(i));
+//			}
+//			
+//			System.out.println(Arrays.toString(parent));
+		}
+		System.out.println(sb);
+	}
+}
