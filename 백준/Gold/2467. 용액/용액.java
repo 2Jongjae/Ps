e7d5443221eb227816e042040ba193cc8b216f05
이종재 <149658209+JongJae2@users.noreply.github.com>
2024-05-25 23:39:43 +0900

    [Gold V] Title: 용액, Time: 396 ms, Memory: 28596 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    	int n = Integer.parseInt(br.readLine());
+    	
+    	int[] arr =new int[n];
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	for(int i = 0; i < n ; i++) {
+    		arr[i] = Integer.parseInt(st.nextToken());
+    	}
+    	
+    	
+    	int mina = 0;
+    	int minb = 0;
+    	int sum = Integer.MAX_VALUE;
+    	int past = Integer.MAX_VALUE;
+    	int left = 0;
+    	int right = n-1;
+    	while(left < right) {
+    		
+    		int a = arr[left] + arr[right];
+    		
+    		
+    		
+    		if( past == a ) {
+    			left++;
+    			continue;
+    		}
+    		
+    		if(Math.abs(a) <sum ) {
+    			mina = left;
+    			minb = right;
+    			sum = Math.abs(a);
+    		}
+    		
+    		if( a< 0 ) left++;
+    		else right --;
+    				
+    		past = a;
+    		
+    	}
+    	System.out.println(arr[mina] + " " + arr[minb]);
+    	
+    	
+    	
+    }
+}
